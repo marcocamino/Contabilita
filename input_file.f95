76821c2726bf3cf458381c6b0debed010202c656
caminoMarco <176792@edu.unito.it>
2020-04-29 23:36:01 +0200

    Leggo un file di 5 colonne di tipo csv

--- input_file.f95
+++ input_file.f95
@@ -6,37 +6,77 @@
 !
 
 MODULE input_file
-    
+    use a_costanti_tipi_module
     implicit none
     
-    type :: met_record
-        integer :: colonna1
-        integer :: colonna2
-        character(len=32) :: str1
-    end type met_record
+!    type :: met_record
+!        integer :: colonna1
+!        integer :: colonna2
+!        character(len=32) :: str1
+!    end type met_record
   
     contains 
     
     !funzione che legge lo scarico dettagliato e lo carica in memoria 
-    function readScaritoTitoliIntoArray()
+!    function readScaritoTitoliIntoArray()
+!        implicit none
+!        integer :: readScaritoTitoliIntoArray
+!        type(met_record), dimension(3) :: weather_reports
+!
+!        integer :: i, ios, fu,j
+!
+!        !lettura di un file csv di cui non conosco il numero di record
+!        print*, "non conosco il numero di record del file csv, lo apro e leggo in modo differente"
+!        open(FILE="C:\Users\audacia\Desktop\fortran\fileInput\fileInput2.csv", &
+!            &  newunit=fu, STATUS="OLD", ACTION="READ",FORM="FORMATTED",POSITION="REWIND")
+!        print*, "leggo e scrivo il programma"
+!        i=1
+!        do
+!            print*, "sono nel ciclo di lettura"
+!            read(fu,*,IOSTAT=ios) weather_reports(i)
+!            if(ios==0) then ! Nessun problema
+!                print*, "letto una riga"
+!                print*, i,">",weather_reports(i),"<"
+!                i = i+1
+!            else ! Problemi in lettura
+!                read(*,*) j                
+!                print*, "termino la lettura problemi"
+!                exit
+!            end if
+!        end do
+!        
+!        
+!        print*, "Stampa del vettore"
+!        do i = 1, 3
+!            print*, weather_reports(i)
+!        end do
+!        close(fu)
+!        read(*,*) j
+!        readScaritoTitoliIntoArray = 0
+!        
+!    end function readScaritoTitoliIntoArray
+    
+    
+!Questa funzione legge il file dello scarico titoli e lo salva in un array.
+        function readScaritoTitoliIntoArray()
         implicit none
         integer :: readScaritoTitoliIntoArray
-        type(met_record), dimension(3) :: weather_reports
+        type(scarico_titoli), dimension(1) :: titolo
 
         integer :: i, ios, fu,j
 
         !lettura di un file csv di cui non conosco il numero di record
         print*, "non conosco il numero di record del file csv, lo apro e leggo in modo differente"
-        open(FILE="C:\Users\audacia\Desktop\fortran\fileInput\fileInput2.csv", &
+        open(FILE="C:\Users\audacia\Desktop\fortran\fileInput\ScaricoTitoloCassa_20181201_20181231_5.csv", &
             &  newunit=fu, STATUS="OLD", ACTION="READ",FORM="FORMATTED",POSITION="REWIND")
         print*, "leggo e scrivo il programma"
         i=1
         do
             print*, "sono nel ciclo di lettura"
-            read(fu,*,IOSTAT=ios) weather_reports(i)
+            read(fu,*,IOSTAT=ios) titolo(1)
             if(ios==0) then ! Nessun problema
                 print*, "letto una riga"
-                print*, i,">",weather_reports(i),"<"
+                print*, i,">",titolo(1),"<"
                 i = i+1
             else ! Problemi in lettura
                 read(*,*) j                
@@ -45,14 +85,15 @@ MODULE input_file
             end if
         end do
         
-        
-        print*, "Stampa del vettore"
-        do i = 1, 3
-            print*, weather_reports(i)
-        end do
-        close(fu)
+        print*, "Il file è composto da: ", i, " righe."
         read(*,*) j
-        readScaritoTitoliIntoArray = 0
+!        print*, "Stampa del vettore"
+!        do i = 1, 3
+!            print*, weather_reports(i)
+!        end do
+!        close(fu)
+!        read(*,*) j
+!        readScaritoTitoliIntoArray = 0
         
     end function readScaritoTitoliIntoArray
 
